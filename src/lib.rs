3675fa5066af2f938fccbd6d6379b4e0a176d7cf
Zachary Whiteley <z.n.whiteley@gmail.com>
2023-01-11 17:47:00 +0000

    Add period module
    Add period module which contains period-related data structures
    (including the Week, TimeSlot, and Period data structures) to allow
    consumers to refer to individual periods within a timetable.
    
    Signed-off-by: Zachary Whiteley <z.n.whiteley@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-pub use ranged::*;
 pub use location::{FearnhillRoom, HighfieldBlock, HighfieldFloor, HighfieldRoom, Location};
+pub use period::{Period, TimeSlot, Week};
+pub use ranged::*;
 
 mod ranged;
 
 /// This module contains data structures which describe the locations of rooms
 /// within the Highfield school and the Fearnhill school.
 mod location;
+
+mod period;
